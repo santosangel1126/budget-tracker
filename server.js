aec5132167042c2d1f672ee9c21be2aff8c2331c
Angel Santos <iiisantosjangel@gmail.com>
2022-08-17 22:46:06 -0700

    fixed syntax errors

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const compression = require('compression')
+
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/budget';
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(compression)();
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndMotify: true
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listenerCount(PORT, () => {
+    console.log(`APP running on port ${PORT}`);
+});
